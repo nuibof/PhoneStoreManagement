80f275276f4cc9d9c5d19cd2d49c001b5bde9bb1
Nam B <nambnps39824@gmail.com>
2026-09-12 22:27:13 +0700

    missing file :(

---
+++ gui/windows/dashboardwindow.cpp
@@ -0,0 +1,143 @@
+//
+// Created by Nam B on 9/12/2026.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_DashboardWindow.h" resolved
+
+#include "dashboardwindow.h"
+#include "ui_DashboardWindow.h"
+#include "../../include/managers/DashboardManager.h"
+#include "../widgets/productspage.h"
+#include "loginwindow.h"
+#include <QPushButton>
+#include <QMessageBox>
+#include <QList>
+#include <QStyle>
+#include <QDebug>
+
+DashboardWindow::DashboardWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::DashboardWindow) {
+    ui->setupUi(this);
+
+    // Add Products Page
+    ProductsPage *productsPage = new ProductsPage();
+    int productsIndex =
+        ui->stackedWidget->addWidget(productsPage);
+
+    // Make Dashboard the default page
+    ui->stackedWidget->setCurrentWidget(ui->dashboardPage);
+
+    // Load dashboard data
+    loadDashboardData();
+
+    auto setActiveButton = [this](QPushButton *activeButton)
+    {
+        QList<QPushButton *> buttons = {
+            ui->btnDashboard,
+            ui->btnProducts,
+            ui->btnCategories,
+            ui->btnCustomers,
+            ui->btnStaff,
+            ui->btnOrders,
+            ui->btnInvoices
+        };
+
+        for (QPushButton *button : buttons)
+        {
+            button->setProperty("active", button == activeButton);
+            button->style()->unpolish(button);
+            button->style()->polish(button);
+        }
+    };
+    // Dashboard (default) active button
+    setActiveButton(ui->btnDashboard);
+    connect(ui->btnDashboard, &QPushButton::clicked, this, [this, setActiveButton]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->dashboardPage);
+        setActiveButton(ui->btnDashboard);
+    });
+
+    // Products
+    connect(ui->btnProducts, &QPushButton::clicked,
+        this, [this, setActiveButton, productsIndex]()
+{
+    ui->stackedWidget->setCurrentIndex(productsIndex);
+    setActiveButton(ui->btnProducts);
+});
+
+    // Categories
+    connect(ui->btnCategories, &QPushButton::clicked, this, [this, setActiveButton]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->categoriesPage);
+        setActiveButton(ui->btnCategories);
+    });
+
+    // Customers
+    connect(ui->btnCustomers, &QPushButton::clicked, this, [this, setActiveButton]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->customersPage);
+        setActiveButton(ui->btnCustomers);
+    });
+
+    // Staff
+    connect(ui->btnStaff, &QPushButton::clicked, this, [this, setActiveButton]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->staffPage);
+        setActiveButton(ui->btnStaff);
+    });
+
+    // Orders
+    connect(ui->btnOrders, &QPushButton::clicked, this, [this, setActiveButton]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->ordersPage);
+        setActiveButton(ui->btnOrders);
+    });
+
+    // Invoices
+    connect(ui->btnInvoices, &QPushButton::clicked, this, [this, setActiveButton]()
+    {
+        ui->stackedWidget->setCurrentWidget(ui->invoicesPage);
+        setActiveButton(ui->btnInvoices);
+    });
+
+    //click btnLogout show yes/no ok back to login window
+    connect(ui->btnLogout, &QPushButton::clicked, this, [this]()
+{
+    QMessageBox::StandardButton reply =
+        QMessageBox::question(
+            this,
+            "Logout",
+            "Chắc chắn muốn đăng xuất?",
+            QMessageBox::Yes | QMessageBox::No
+        );
+
+    if (reply == QMessageBox::Yes)
+    {
+        auto *loginWindow = new LoginWindow();
+        loginWindow->show();
+        close();
+    }
+});
+}
+
+DashboardWindow::~DashboardWindow() {
+    delete ui;
+}
+
+void DashboardWindow::loadDashboardData()
+{
+    qDebug() << "=== Loading Dashboard Data ===";
+
+    DashboardManager manager;
+
+    int productCount = manager.getProductCount();
+    int orderCount = manager.getOrderCount();
+    int customerCount = manager.getCustomerCount();
+
+    qDebug() << "Products:" << productCount;
+    qDebug() << "Orders:" << orderCount;
+    qDebug() << "Customers:" << customerCount;
+
+    ui->productsValue->setText(QString::number(productCount));
+    ui->ordersValue->setText(QString::number(orderCount));
+    ui->customersValue->setText(QString::number(customerCount));
+}
